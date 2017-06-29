84d25f23afe60bb3ad4cfef2a3ab19c5c7812008
Jerey-Jobs <610315802@qq.com>
2017-06-29 18:02:17 +0800

    增加矢量图

--- app/src/main/java/cn/jerey/sherlockscrollanimation/MainActivity.java
+++ app/src/main/java/cn/jerey/sherlockscrollanimation/MainActivity.java
@@ -1,7 +1,12 @@
 package cn.jerey.sherlockscrollanimation;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.path_iv);
+        Drawable drawable = imageView.getDrawable();
+        if(drawable instanceof Animatable){
+            ((Animatable)drawable).start();
+        }
+
     }
 }
